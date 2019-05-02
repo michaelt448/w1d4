1385ddd10faa596308acece003debb258046eaeb
Michael Tilman <mikegithub77@gmail.com>
2019-05-02 18:57:44 +0000

    sorting algorithem which sorts students only by age

---
+++ modified-sorted.js
@@ -0,0 +1,19 @@
+
+var result = students.sort(function(a,b) {
+  return a.age > b.age ? a:b;
+})
+
+
+
+
+
+
+
+
+
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
