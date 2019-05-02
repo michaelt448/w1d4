32603599133b9bd9f9bdcea5ee895baa4a7f4636
Michael Tilman <mikegithub77@gmail.com>
2019-05-02 19:15:42 +0000

    added functionality to sort first alphabetically and than by age

--- modified-sorted.js
+++ modified-sorted.js
@@ -1,19 +1,27 @@
-
-var result = students.sort(function(a,b) {
-  return a.age > b.age ? a:b;
-})
-
-
-
-
-
-
-
-
-
 var students = [
   { id: 1, name: "bruce",    age: 40 },
   { id: 2, name: "zoidberg", age: 22 },
   { id: 3, name: "alex",     age: 22 },
   { id: 4, name: "alex",     age: 30 }
 ];
+
+var sortByAge = function(studentA,studentB) {
+  return studentA.age - studentB.age ;
+}
+
+var sortByName = students.sort(function(a,b){
+  if(a.name === b.name) {
+    return sortByAge(a,b);
+  }
+  return a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1;
+});
+
+
+console.log(sortByName);
+
+
+
+
+
+
+
